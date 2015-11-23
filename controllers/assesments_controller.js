d775b051eeb3579772796a803ba2279dd329aa9e
myteddy15 <lattedphilip@gmail.com>
2015-11-23 11:22:33 +0800

    Assesment module interface

--- controllers/assesments_controller.js
+++ controllers/assesments_controller.js
@@ -7,6 +7,8 @@ define(['app','api'], function (app) {
 			$scope.hasBasicInfo=false;
 			$scope.hasContactInfo=false;
 			$scope.ActiveStep=1;
+			$scope.SelectedStudent={};
+			$scope.ActiveStudent={};
 			};
 			$scope.init();
 			$scope.Steps = [
@@ -17,36 +19,39 @@ define(['app','api'], function (app) {
 				{id:5, description:"Select Discount"},
 				{id:6, description:"Confirmation"}
 			];
+			$scope.Students=[];
+			api.GET('students',function success(response){
+				console.log(response.data);
+				$scope.Students = response.data;
+			});
 			$scope.YearLevels=[];
 			api.GET('year_levels',function success(response){
 				console.log(response.data);
 				$scope.YearLevels = response.data;
 			});
-			$scope.Countries=[];
-			api.GET('countries',function success(response){
+			$scope.Sections=[];
+			api.GET('sections',function success(response){
 				console.log(response.data);
-				$scope.Countries = response.data;
+				$scope.Sections = response.data;
 			});
-			$scope.Provinces=[];
-			api.GET('provinces',function success(response){
+			$scope.PaymentSchemes=[];
+			$scope.Tuitions=[];
+			api.GET('tuition',function success(response){
 				console.log(response.data);
-				$scope.Provinces = response.data;
-			});
-			$scope.Cities=[];
-			api.GET('cities',function success(response){
-				console.log(response.data);
-				$scope.Cities = response.data;
+				$scope.Tuitions = response.data;
+				$scope.PaymentSchemes=$scope.Tuitions[0].schemes;
+				
 			});
 			$scope.nextStep = function(){
-			if($scope.ActiveStep===1){
-				$scope.basicInfo();
-			}
-			if($scope.ActiveStep===2){
-				$scope.contactInfo();
-			}
-			if($scope.ActiveStep===3){
-				$scope.sendInfo();
-			};
+			//if($scope.ActiveStep===1){
+				//$scope.ActiveStudent = $scope.SelectedStudent;
+			//}
+			//if($scope.ActiveStep===2){
+				//$scope.contactInfo();
+			//}
+			//if($scope.ActiveStep===3){
+				//$scope.sendInfo();
+			//};
 			if($scope.ActiveStep<$scope.Steps.length){
 				$scope.ActiveStep++;
 			}
@@ -56,92 +61,15 @@ define(['app','api'], function (app) {
 					$scope.ActiveStep--;
 				};
 			};
-			$scope.getId = function(department){
-				$scope.educID=department.id;
-				//console.log($scope.educID);
-			};
-			$scope.getGender=function(gender){
-				$scope.gender = gender;
-			};
 			$scope.updateStep=function(step){
 				$scope.ActiveStep = step.id;
 			};
-			$scope.basicInfo=function(){
-				$scope.Student.educ_level_id=$scope.educID;
-				$scope.Student.year_level_id=$scope.level;
-				$scope.Student.first_name=$scope.firstName;
-				$scope.Student.middle_name=$scope.middleName;
-				$scope.Student.last_name=$scope.lastName;
-				$scope.Student.suffix_name=$scope.suffix;
-				$scope.Student.gender=$scope.gender;
-				$scope.Student.birthday=$scope.birthday;
-				$scope.Student.birthplace=$scope.birthPlace;
-				$scope.Student.religion=$scope.religion;
-				$scope.Student.citizenship=$scope.citizenship;
-				$scope.hasBasicInfo = true;
-			};
-			$scope.contactInfo=function(){
-				$scope.Student.contact_numbers=[];
-				var landline = {type:'landline', number:$scope.landline};
-				var mobile = {type:'mobile', number:$scope.mobile};
-				$scope.Student.contact_numbers.push(landline);
-				$scope.Student.contact_numbers.push(mobile);
-				var current = {	type:'current',
-								country:$scope.currentCountry,
-								province:$scope.currentProvince,
-								city:$scope.currentCity,
-								barangay:$scope.currentBrgy,
-								address:$scope.currentAddrs,
-								};
-				var permanent = {type:'permanent',
-								country:$scope.homeCountry,
-								province:$scope.homeProvince,
-								city:$scope.homeCity,
-								barangay:$scope.homeBrgy,
-								address:$scope.homeAddrs,
-								};
-				$scope.Student.addressess=[];
-				$scope.Student.addressess.push(current);
-				$scope.Student.addressess.push(permanent);
-				$scope.hasContactInfo = true;
-			};
-			$scope.sendInfo = function(){
-				api.POST('students',$scope.Student,function success(response){
-				console.log(response.data);
-				$scope.init();
-				$scope.clearField();
-				$scope.clearField2();
-				//reset student info and step
-				//call init
-				//clearField clearField2
-			});
-			};
-			$scope.clearField=function(){
-				$scope.educID = null;
-				$scope.level = null;
-				$scope.firstName = null;
-				$scope.middleName = null;
-				$scope.lastName = null;
-				$scope.suffix = null;
-				$scope.gender = null;
-				$scope.birthday = null;
-				$scope.birthPlace = null;
-				$scope.religion = null;
-				$scope.citizenship = null;
-			};
-			$scope.clearField2=function(){
-				$scope.landline = null;
-				$scope.mobile = null;
-				$scope.currentCountry = null;
-				$scope.currentProvince = null;
-				$scope.currentCity = null;
-				$scope.currentBrgy = null;
-				$scope.currentAddrs = null;
-				$scope.homeCountry = null;
-				$scope.homeProvince = null;
-				$scope.homeCity = null;
-				$scope.homeBrgy = null;
-				$scope.homeAddrs = null;
+			$scope.setSelecetedStudent=function(student){
+				$scope.SelectedStudent = {
+										 id:student.id,
+										 name:student.first_name+" "+student.middle_name+" "+student.last_name+" "+student.suffix_name,
+										 yearlevel:student.year_level_id
+				                         };
 			};
 		};
     }]);
