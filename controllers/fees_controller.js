a96bd3e797f3b8d8a80e6f44194a4d39586e664b
myteddy15 <lattedphilip@gmail.com>
2015-12-01 10:40:22 +0800

    Add Update state function

--- controllers/fees_controller.js
+++ controllers/fees_controller.js
@@ -2,72 +2,75 @@
 define(['app','api'], function (app) {
     app.register.controller('FeeController',['$scope','$rootScope','api', function ($scope,$rootScope,api) {
 		$scope.init=function(){
-			$scope.SS=[
+			$scope.Spreadsheet=[
 						[
-						{
-							row:1,
-							col:2,
-							state: "read",
-							value:"A"
-						},
-						{
-							row:1,
-							col:2,
-							state: "read",
-							value:"B"
-						},
-						{
-							row:1,
-							col:3,
-							state: "read",
-							value:"C"
-						
-						
-							row:1,
-							col:4,
-							state: "read",
-							value:"D"
-						},
-						{
-							row:2,
-							col:5,
-							state: "read",
-							value:"E"
-						},
+							{
+								row:1,
+								col:2,
+								state: "read",
+								value:"A"
+							},
+							{
+								row:1,
+								col:2,
+								state: "read",
+								value:"B"
+							},
+							{
+								row:1,
+								col:3,
+								state: "read",
+								value:"C"
+							},
+							{
+								row:1,
+								col:4,
+								state: "read",
+								value:"D"
+							},
+							{
+								row:2,
+								col:5,
+								state: "read",
+								value:"E"
+							},
 						],
 						[
-						{
-							row:2,
-							col:2,
-							state: "read",
-							value:"F"
-						},
-						{
-							row:2,
-							col:2,
-							state: "read",
-							value:"G"
-						},
-						{
-							row:2,
-							col:3,
-							state: "read",
-							value:"H"
-						
-						
-							row:2,
-							col:4,
-							state: "read",
-							value:"I"
-						},
-						{
-							row:2,
-							col:5,
-							state: "read",
-							value:"J"
-						},
+							{
+								row:2,
+								col:2,
+								state: "write",
+								value:"F"
+							},
+							{
+								row:2,
+								col:2,
+								state: "write",
+								value:"G"
+							},
+							{
+								row:2,
+								col:3,
+								state: "write",
+								value:"H"
+							},
+							{
+								row:2,
+								col:4,
+								state: "write",
+								value:"I"
+							},
+							{
+								row:2,
+								col:5,
+								state: "write",
+								value:"J"
+							},
 						]
 					  ];
+		};
+		$scope.updateState=function(rowIndex,colIndex,state){
+			$scope.Spreadsheet[rowIndex][colIndex].state=state;
 		};
     }]);
 });
