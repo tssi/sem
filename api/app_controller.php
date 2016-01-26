67b4a1915abdfea9a6c3cf8ef45c4876a5370ab4
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-01-26 17:09:17 +0800

    Implement json parsing and response

--- api/app_controller.php
+++ api/app_controller.php
@@ -31,4 +31,47 @@
  * @subpackage    cake.app
  */
 class AppController extends Controller {
+	var $components = array('RequestHandler','Session');
+	var $helpers = array('Html','Form','Api');
+	function beforeFilter() {
+		if ($this->RequestHandler->ext === 'json'){
+			$this->RequestHandler->setContent('json', 'application/json');
+			//Configure::write('debug', 0);
+			$endpoint = $this->RequestHandler->params['controller'];
+			$__Class = Inflector::classify($endpoint);
+			$Endpoint = &ClassRegistry::init($__Class);
+			$conf = array();
+			//Pagination config
+			$page = isset($_GET['page'])?$_GET['page']:1;
+			$limit = $conf['limit'] = isset($_GET['limit'])?$_GET['limit']:null;
+			$recursive =  -1;
+			$offset = $conf['offset'] = $page&&$limit?($page-1)*$limit:null;
+			//Pagination count
+			$count_conf = $conf;
+			unset($count_conf['limit']);
+			unset($count_conf['offset']);
+			$count = $Endpoint->find('count',$count_conf);
+			$last = $limit?ceil($count/$limit):1;
+			$next = $page < $last ? $page + 1:null;
+			$prev = $page>1?$page - 1:null;
+			//Meta Data
+			$meta = array();
+			$page_url = null;
+			$meta['title'] = $__Class;
+			$meta['next'] = $next? $page_url.$next:null;
+			$meta['prev'] = $prev? $page_url.$prev:null;
+			$meta['last'] = $page_url.$last;
+			$meta['items'] = $count;
+			$meta['pages'] = $last;
+			//Set up paginate
+			$paginate = array();
+			$paginate['page'] = $page;
+			$paginate['cache'] = 'default';
+			if($limit) $paginate['limit']=$limit;
+			if($recursive) $paginate['recursive']=$recursive;
+			$this->paginate = array($__Class => $paginate);
+			$this->set(compact('meta'));
+		}
+		return parent::beforeFilter();
+	}
 }
