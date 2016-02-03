9dd761b45964455059d1e5090a376702580ad3c7
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-02-03 16:12:58 +0800

    Update maintenance list controller and view

--- controllers/maintenance_controller.js
+++ controllers/maintenance_controller.js
@@ -3,17 +3,12 @@ define(['app','api'], function (app) {
     app.register.controller('MaintenanceController',['$scope','$rootScope','$uibModal','api', function ($scope,$rootScope,$uibModal,api) {
 		$scope.list=function(){
 			$rootScope.__MODULE_NAME = 'Maintenance';
-			$scope.MaintenanceList=[
-									{id:1, name:"Fees", description:"List of Fees", path:"fees"},
-									{id:2, name:"Year Level", description:"List of Year Level", path:"year_levels"},
-									{id:3, name:"Section", description:"List of Section", path:"sections"},
-									{id:4, name:"Religion", description:"List of Religion", path:"religions"},
-									{id:5, name:"Citizenship", description:"List of Citizenship", path:"citizenships"}
-								   ];
+			initAPIRequests();
 			$scope.newItem={};
 			$scope.$watchCollection('SortItem',function(newValue,oldValue){
 				$scope.sortItems();
 			});
+			
 	   };
 		$scope.openMaintenance=function(list){
 			$scope.List=angular.copy(list);
@@ -21,16 +16,22 @@ define(['app','api'], function (app) {
 			$scope.Columns=[];
 			$scope.ErrorMessage  = null;
 			$scope.ErrorCode  = null;
-			api.GET(list.path,{limit:25},function success(response){
-				$scope.ListItems=response.data;
-				for(var key in response.data[0]){
+			if(typeof list.schema=='object'){
+				for(var index in list.schema){
+					var key =  list.schema[index];
 					if(key!="order"&&key!="created"&&key!="modified")
 							$scope.Columns.push(key);
 				};
-				$scope.ColumnLen =  Math.round(10/($scope.Columns.length));
+			}
+			$scope.ColumnLen =  Math.round(10/($scope.Columns.length));
+			api.GET(list.path,function success(response){
+				$scope.ListItems=response.data;
 			},function error(response){
+				console.log(response);
+				$scope.ListItems = [];
 				$scope.ErrorMessage =  response.meta.message;
 				$scope.ErrorCode = response.meta.code;
+				
 			});
 		};
 		$scope.removeMaintenanceInfo=function(){
@@ -62,12 +63,18 @@ define(['app','api'], function (app) {
 			api.PUT($scope.List.path,$scope.NewItem,function success(response){
 			});
 		};
-		api.GET('year_levels',{limit:15},function success(response){
+		function initAPIRequests()
+		{
+			api.GET('maintenance_lists',function success(response){
+				$scope.MaintenanceList = response.data;
+			});	
+			api.GET('year_levels',function success(response){
 			$scope.YearLevels = response.data;
-		});
-		api.GET('educ_levels',{limit:15},function success(response){
-			$scope.EducLevels = response.data;
-		});
+			});	
+			api.GET('educ_levels',function success(response){
+				$scope.EducLevels = response.data;
+			});
+		}
 		$scope.sortItems=function(){
 			for(var index in $scope.SortItem){
 				$scope.SortItem[index].order = parseInt(index)+1;
