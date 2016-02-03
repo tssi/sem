6867b3a21d344c2334d1037d7e08c7be7f6d16d2
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-02-03 16:11:46 +0800

    Update error handling

--- api/app_error.php
+++ api/app_error.php
@@ -1,12 +1,22 @@
 <?php
 class AppError extends ErrorHandler {
+	function invalidEndpoint($params){
+		$code ='403';
+		$message = sprintf('Invalid Endpoint');
+		$this->fetchError($code,$message);
+	}
+	function dataNotSet($params){
+		$code ='402';
+		$message = sprintf('Data not set');
+		$this->fetchError($code,$message);
+	}
 	function emptyRecord($params){
 		$code ='404';
 		$message = sprintf('Empty Record Found');
 		$this->fetchError($code,$message);
 	}
 	protected function fetchError($code,$message){
-		$this->controller->set(compact('id','code','message'));
+		$this->controller->set(compact('code','message'));
 		$this->controller->layout='json/default';
 		$this->_outputMessage('json/default');
 	}
