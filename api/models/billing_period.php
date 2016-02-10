70ee260ccc29e814571696dac59fbac4d6520758
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-02-10 10:02:14 +0800

    Compute billing period due dates

--- api/models/billing_period.php
+++ api/models/billing_period.php
@@ -11,14 +11,18 @@ class BillingPeriod extends AppModel {
 			$pay_freq	= $bill_period['payment_frequency'];
 			$cutoff		= $bill_period['bill_cutoff_date'];
 			$cycle		= $bill_period['bill_cycle_increment'];
-			$due_dates	= array();
+			$due_dates			= array();
+			$bill_months		= array();
 			$time 		= strtotime($sy.'-'.$mon_start.'-'.$cutoff);
 			for($ctr=1;$ctr<=$pay_freq;$ctr++){
 				$due_date = date('Y-m-d',$time);
+				$bill_month = date('M Y',$time);
 				$time = strtotime($due_date.' '.$cycle);
 				array_push($due_dates,$due_date);
+				array_push($bill_months,$bill_month);
 			}
 			$bill_period['due_dates'] = $due_dates;
+			$bill_period['bill_months'] = $bill_months;
 			$billingPeriodList[$bill_period['id']] = $bill_period;
 		}
 		return $billingPeriodList;
