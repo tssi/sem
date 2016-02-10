8dc8955dc9d0ab7e039a9252d2eb123b0fb21ae3
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-02-10 10:28:36 +0800

    Add virtual field display_amount

--- api/models/discount.php
+++ api/models/discount.php
@@ -1,4 +1,11 @@
 <?php
 class Discount extends AppModel {
 	var $name = 'Discount';
+	var $virtualFields = array('display_amount'=>
+					"CASE Discount.type 
+						WHEN  'percent' THEN 
+							CONCAT(CAST(Discount.amount AS DECIMAL) ,'%')
+						ELSE
+							CONCAT('P',Discount.amount)
+					END");
 }
