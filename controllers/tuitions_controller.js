53d0a5e57153ab0cdfc42003aa17eef0ac730c94
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-02-18 18:38:29 +0800

    Payment Scheme Schedule saving

--- controllers/tuitions_controller.js
+++ controllers/tuitions_controller.js
@@ -27,6 +27,12 @@ define(['app','api'], function (app) {
 						$scope.saveTuitionTotal();
 					}
 			}, true);
+			$scope.$watch('Tuition.amount',function(newAmount){
+				 for(var i in $scope.Schemes){
+					   var scheme =  $scope.Schemes[i];
+					   $scope.Variance[scheme.id]= $scope.Totals[scheme.id] - newAmount;
+				   }
+			});
 	   };
 	   $scope.saveTuitionTotal = function(){
 			var data = {id:$scope.Tuition.id,amount:$scope.Tuition.amount};
@@ -42,6 +48,7 @@ define(['app','api'], function (app) {
 		   $scope.SavingFee = [];
 		   initAmounts();
 		   initTotals();
+		   mapAmounts();
 	   }
 	   $scope.removeTuitionInfo = function(){
 		   $scope.Tuition = null;
@@ -134,7 +141,7 @@ define(['app','api'], function (app) {
 				$scope.SavingFees =false;
 			});
 		};
-		$scope.computeTotal = function(scheme_id){
+		$scope.computeTotal = function(scheme_id, billing_period_id){
 			$scope.Totals[scheme_id]=0;
 			for(var bill_period in $scope.Amounts){
 				var amount = $scope.Amounts[bill_period][scheme_id];
@@ -142,12 +149,30 @@ define(['app','api'], function (app) {
 				if(amount)
 					$scope.Totals[scheme_id]+=(amount*multiplyer);
 			}
+			$scope.Variance[scheme_id] =  $scope.Totals[scheme_id] - $scope.Tuition.amount;
+			if(billing_period_id){
+				var tuition_id = $scope.Tuition.id;
+				var payment_scheme_id = $scope.SchemeId[billing_period_id][scheme_id];
+				var schedule_id = $scope.ScheduleId[billing_period_id][scheme_id];
+				var total_amount  = $scope.Totals[scheme_id];
+				var variance_amount  = $scope.Variance[scheme_id];
+				var amount = $scope.Amounts[billing_period_id][scheme_id];
+					if(amount==undefined) amount = 0;
+				var scheme_data = {id:payment_scheme_id,tuition_id:tuition_id,scheme_id:scheme_id,total_amount:total_amount,variance_amount:variance_amount};
+				api.PUT('payment_schemes',scheme_data,function success(response){
+					$scope.SchemeId[billing_period_id][scheme_id] =  response.data.id;
+				});
+				var schedule_data = {id:schedule_id,tuition_id:tuition_id,scheme_id:scheme_id,billing_period_id:billing_period_id,amount:amount};
+				api.PUT('payment_scheme_schedules',schedule_data,function success(response){
+					$scope.ScheduleId[billing_period_id][scheme_id] =  response.data.id;
+				});
+			}
 		}
 		$scope.resetAmounts = function(bill_period){
 			$scope.Amounts[bill_period] = {};
 			for(var i in $scope.Schemes){
 				var scheme = $scope.Schemes[i];
-				$scope.computeTotal(scheme.id);
+				$scope.computeTotal(scheme.id,bill_period);
 			}
 		}
 		function resetTuition(){
@@ -180,18 +205,41 @@ define(['app','api'], function (app) {
 	   function initAmounts(){
 		   $scope.Amounts = {};
 		   $scope.Multiplyer = {};
+		   $scope.SchemeCode = {};
+		   $scope.ScheduleId = {};
+		   $scope.SchemeId = {};
 		   for(var i in $scope.BillingPeriods){
 			   var period =  $scope.BillingPeriods[i];
+			   $scope.ScheduleId[period.id]={};
+			   $scope.SchemeId[period.id]={};
 			   $scope.Amounts[period.id]={};
 			   $scope.Multiplyer[period.id]=period.payment_frequency;
+			   $scope.SchemeCode[period.id]=period.code;
 		   }
 	   }
 	   function initTotals(){
 		   $scope.Totals = {};
+		   $scope.Variance = {};
 		   for(var i in $scope.Schemes){
 			   var scheme =  $scope.Schemes[i];
 			   $scope.Totals[scheme.id]=0;
+			   if($scope.Tuition) 
+					$scope.Variance[scheme.id]=-$scope.Tuition.amount;
+			   else
+					$scope.Variance[scheme.id] = 0;
 		   }
+	   }
+	   function mapAmounts(){
+		   for(var si in $scope.Tuition.schemes){
+				var scheme  =  $scope.Tuition.schemes[si];
+				for(var sj in scheme.schedule){
+				   var schedule =  scheme.schedule[sj];
+					$scope.SchemeId[schedule.billing_period_id][scheme.id] = schedule.payment_scheme_id;
+					$scope.ScheduleId[schedule.billing_period_id][scheme.id] = schedule.id;
+					$scope.Amounts[schedule.billing_period_id][scheme.id] = schedule.amount;
+				}
+				$scope.computeTotal(scheme.id);
+		   }		   
 	   }
 	}]);
 });
