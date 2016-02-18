afa6bc52cbce37810b1e87e589dc9ad850ae066c
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-02-18 18:36:35 +0800

    Create custom functions for billing periods model

--- api/models/billing_period.php
+++ api/models/billing_period.php
@@ -2,29 +2,37 @@
 class BillingPeriod extends AppModel {
 	var $name = 'BillingPeriod';
 	var $order = 'BillingPeriod.order';
-	function getDueDates($sy){
-		$billingPeriodList =  $this->find('list');
-		$billingPeriods =  $this->find('all');
+	function getDueDates($sy,$period=null){
+		$conditions = array();
+		if($period)
+			$conditions['BillingPeriod.id']=$period;
+		$billingPeriodList =  $this->find('list',compact('conditions'));
+		$billingPeriods =  $this->find('all',compact('conditions'));
 		foreach($billingPeriods as $index=>$_BP){
 			$bill_period= $_BP['BillingPeriod'];
 			$mon_start	= $bill_period['bill_month_start'];
 			$pay_freq	= $bill_period['payment_frequency'];
 			$cutoff		= $bill_period['bill_cutoff_date'];
 			$cycle		= $bill_period['bill_cycle_increment'];
-			$due_dates			= array();
-			$bill_months		= array();
+			$due_dates	= array();
 			$time 		= strtotime($sy.'-'.$mon_start.'-'.$cutoff);
 			for($ctr=1;$ctr<=$pay_freq;$ctr++){
 				$due_date = date('Y-m-d',$time);
-				$bill_month = date('M Y',$time);
 				$time = strtotime($due_date.' '.$cycle);
 				array_push($due_dates,$due_date);
-				array_push($bill_months,$bill_month);
 			}
 			$bill_period['due_dates'] = $due_dates;
-			$bill_period['bill_months'] = $bill_months;
 			$billingPeriodList[$bill_period['id']] = $bill_period;
 		}
 		return $billingPeriodList;
+	}
+	function getBillMonths($due_dates){
+		$bill_months = array();
+		foreach($due_dates as $due_date){
+			$time =  strtotime($due_date);
+			$bill_month = date('M Y',$time);
+			array_push($bill_months,$bill_month);
+		}
+		return $bill_months;
 	}
 }
