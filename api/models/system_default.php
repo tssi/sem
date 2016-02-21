f4122bd58783eff5e0bf21b72c4ccca27c8a73e9
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-02-21 10:52:40 +0800

    Modify SystemDefault data

--- api/models/system_default.php
+++ api/models/system_default.php
@@ -1,4 +1,33 @@
 <?php
 class SystemDefault extends AppModel {
 	var $name = 'SystemDefault';
+	function afterFind($results){
+		if(isset($results[0]))
+			if(isset($results[0]['SystemDefault'])){
+				$_result = array();
+				foreach($results as $result){
+					$value = json_decode($result['SystemDefault']['value'],true);
+					if(json_last_error()==JSON_ERROR_NONE)
+						$_result[$result['SystemDefault']['key']] = $value;
+					else
+						$_result[$result['SystemDefault']['key']] = $result['SystemDefault']['value'];
+					
+					if(isset($_result['START_SY']) && isset($_result['ACTIVE_SY']) && !isset($_result['SCHOOL_YEARS'])){
+						$school_years = array();
+						for($sy = $_result['START_SY'];$sy<=$_result['ACTIVE_SY'];$sy++){
+							$school_year = array(
+										'id'=>$sy,
+										'label'=>$sy.'-'.($sy+1),
+										'code'=>substr($sy,-2),
+							);
+							array_push($school_years,$school_year);
+						}
+						$_result['SCHOOL_YEARS'] = $school_years;
+					}
+				}
+				
+				$results = array(array('SystemDefault'=>$_result));
+			}
+		return $results;
+	}
 }
