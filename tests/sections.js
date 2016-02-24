4423329427127e4247c97786fd90ca28445ddf75
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-02-24 08:54:14 +0800

    Adjust tests/sections.js

--- tests/sections.js
+++ tests/sections.js
@@ -2,53 +2,108 @@
 define(['model'],function($model){
 	return new $model(
 			{
-				meta:{
-					title: 'Sections',
+			  "meta": {
+				"message": "List of Sections",
+				"limit": 2000,
+				"next": null,
+				"prev": null,
+				"last": 1,
+				"count": 8,
+				"page": 1,
+				"pages": 1,
+				"epoch": 1456275222,
+				"code": 200
+			  },
+			  "data": [
+				{
+				  "id": 3,
+				  "year_level_id": "G7",
+				  "name": "Emerald",
+				  "program_id": "REG",
+				  "order": 1,
+				  "created": "2016-02-22 04:59:02",
+				  "modified": "2016-02-22 04:59:08",
+				  "program": "Regular",
+				  "year_level": "Grade 7"
 				},
-				data:[
-					{
-					  "id": 1,
-					  "name": "Emerald",
-					  "year_level_id": "G7",
-					  "program": "pilot",
-					  "order": 1
-					},
-					{
-					  "id": 2,
-					  "name": "Sapphire",
-					  "year_level_id": "G7",
-					  "program": "regular",
-					  "order": 2
-					},
-					{
-					  "id": 3,
-					  "name": "Gold",
-					  "year_level_id": "G8",
-					  "program": "pilot",
-					  "order": 3
-					},
-					{
-					  "id": 4,
-					  "name": "Silver",
-					  "year_level_id": "G8",
-					  "program": "regular",
-					  "order": 4
-					},
-					{
-					  "id": 5,
-					  "name": "Earth",
-					  "year_level_id": "G9",
-					  "program": "pilot",
-					  "order": 5
-					},
-					{
-					  "id": 6,
-					  "name": "Mars",
-					  "year_level_id": "G9",
-					  "program": "regular",
-					  "order": 6
-					}
-				]
+				{
+				  "id": 6,
+				  "year_level_id": "G8",
+				  "name": "Camia",
+				  "program_id": "REG",
+				  "order": 4,
+				  "created": "2016-02-24 00:52:29",
+				  "modified": "2016-02-24 00:52:29",
+				  "program": "Regular",
+				  "year_level": "Grade 8"
+				},
+				{
+				  "id": 8,
+				  "year_level_id": "G9",
+				  "name": "Silver",
+				  "program_id": "REG",
+				  "order": 6,
+				  "created": "2016-02-24 00:52:56",
+				  "modified": "2016-02-24 00:52:56",
+				  "program": "Regular",
+				  "year_level": "Grade 9"
+				},
+				{
+				  "id": 10,
+				  "year_level_id": "GX",
+				  "name": "Bonifacio",
+				  "program_id": "REG",
+				  "order": 8,
+				  "created": "2016-02-24 00:53:21",
+				  "modified": "2016-02-24 00:53:31",
+				  "program": "Regular",
+				  "year_level": "Grade 10"
+				},
+				{
+				  "id": 4,
+				  "year_level_id": "G7",
+				  "name": "Ruby",
+				  "program_id": "SCI",
+				  "order": 2,
+				  "created": "2016-02-24 00:52:06",
+				  "modified": "2016-02-24 00:52:06",
+				  "program": "Science",
+				  "year_level": "Grade 7"
+				},
+				{
+				  "id": 5,
+				  "year_level_id": "G8",
+				  "name": "Sampaguita",
+				  "program_id": "SCI",
+				  "order": 3,
+				  "created": "2016-02-24 00:52:19",
+				  "modified": "2016-02-24 00:52:19",
+				  "program": "Science",
+				  "year_level": "Grade 8"
+				},
+				{
+				  "id": 7,
+				  "year_level_id": "G9",
+				  "name": "Gold",
+				  "program_id": "SCI",
+				  "order": 5,
+				  "created": "2016-02-24 00:52:49",
+				  "modified": "2016-02-24 00:52:49",
+				  "program": "Science",
+				  "year_level": "Grade 9"
+				},
+				{
+				  "id": 9,
+				  "year_level_id": "GX",
+				  "name": "Rizal",
+				  "program_id": "SCI",
+				  "order": 7,
+				  "created": "2016-02-24 00:53:13",
+				  "modified": "2016-02-24 00:53:25",
+				  "program": "Science",
+				  "year_level": "Grade 10"
+				}
+			  ]
 			}
-		);
+	);
 });
