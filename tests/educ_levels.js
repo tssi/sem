9c8e9388dc0e0fe165ee2da83f0eb11d4d871a82
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-02-24 08:49:08 +0800

    Adjust tests/educ_levels.js

--- tests/educ_levels.js
+++ tests/educ_levels.js
@@ -2,26 +2,52 @@
 define(['model'],function($model){
 	return new $model(
 			{
-				meta:{
-					title: 'Deparments',
+			  "meta": {
+				"message": "List of Educ Levels",
+				"limit": 10,
+				"next": null,
+				"prev": null,
+				"last": 1,
+				"count": 4,
+				"page": 1,
+				"pages": 1,
+				"epoch": 1456274897,
+				"code": 200
+			  },
+			  "data": [
+				{
+				  "id": "PS",
+				  "name": "Preschool",
+				  "alias": "PS",
+				  "order": 1,
+				  "created": "2016-01-26 07:25:49",
+				  "modified": "2016-01-27 01:22:58"
 				},
-				data:[
-					  {
-						"id": "PS",
-						"name": "Preschool",
-						"alias": "Preschool"
-					  },
-					  {
-						"id": "GS",
-						"name": "Grade School",
-						"alias": "Grade S."
-					  },
-					  {
-						"id": "HS",
-						"name": "High School",
-						"alias": "High S."
-					  }
-					]
+				{
+				  "id": "GS",
+				  "name": "Grade School",
+				  "alias": "GS",
+				  "order": 2,
+				  "created": "2016-01-26 07:25:41",
+				  "modified": "2016-01-27 01:22:50"
+				},
+				{
+				  "id": "HS",
+				  "name": "High School",
+				  "alias": "HS",
+				  "order": 3,
+				  "created": "2016-01-26 07:25:56",
+				  "modified": "2016-01-27 01:22:54"
+				},
+				{
+				  "id": "SH",
+				  "name": "Senior High",
+				  "alias": "SH",
+				  "order": 4,
+				  "created": "2016-02-04 08:20:11",
+				  "modified": "2016-02-04 08:20:11"
+				}
+			  ]
 			}
 		);
 });
