caa94ac4c6a61fe829a0f18cfd48034fa194ac33
Dave Arroyo-Alaras <arroyo.daveil@gmail.com>
2016-02-24 08:38:17 +0800

    Adjust test students

--- tests/students.js
+++ tests/students.js
@@ -2,61 +2,54 @@
 define(['model'],function($model){
 	return new $model(
 			{
-				meta:{
-					title: 'Students',
+			  "meta": {
+				"message": "List of Students",
+				"limit": 10,
+				"next": null,
+				"prev": null,
+				"last": 1,
+				"count": 2,
+				"page": 1,
+				"pages": 1,
+				"epoch": 1456274154,
+				"code": 200
+			  },
+			  "data": [
+				{
+				  "id": 1,
+				  "educ_level_id": "HS",
+				  "year_level_id": "G7",
+				  "first_name": "Juan",
+				  "middle_name": "Masipag",
+				  "last_name": "Dela Cruz",
+				  "suffix_name": "II",
+				  "gender": "M",
+				  "birthday": "1998-09-29",
+				  "birthplace": "Batangas",
+				  "religion": "Iglesia ni Cristo",
+				  "citizenship": "Filipino",
+				  "prev_school": "Basic Education School",
+				  "created": "2016-02-24 00:29:27",
+				  "modified": "2016-02-24 00:29:27"
 				},
-				data:[
-					{
-					  "id":"S201512345",
-					  "educ_level_id": "GS",
-					  "year_level_id": "G7",
-					  "first_name": "Juan",
-					  "middle_name": "Masipag",
-					  "last_name": "Dela Cruz",
-					  "suffix_name": "Jr",
-					  "gender": "M",
-					  "birthday": "1990-01-01",
-					  "birthplace": "Malvar",
-					  "religion": "Roman Catholic",
-					  "citizenship": "Filipino",
-					  "prev_school": "ICMS",
-					  "family": [
-						{
-						  "type": "parent",
-						  "name": "1234567",
-						  "occupation": "Programmer"
-						}
-					  ],
-					  "contact_numbers": [
-						{
-						  "type": "landline",
-						  "number": "1234567"
-						},
-						{
-						  "type": "mobile",
-						  "number": "09171234567"
-						}
-					  ],
-					  "addresses": [
-						{
-						  "type": "current",
-						  "country": "PH",
-						  "province": "BTG",
-						  "city": "Santo Tomas",
-						  "barangay": "San Roque",
-						  "address": "Dona Fidela"
-						},
-						{
-						  "type": "permanent",
-						  "country": "PH",
-						  "province": "BTG",
-						  "city": "Balayan",
-						  "barangay": "Gimalas",
-						  "address": "123"
-						}
-					  ]
-					}
-				]
+				{
+				  "id": 2,
+				  "educ_level_id": "GS",
+				  "year_level_id": "G5",
+				  "first_name": "Juanita",
+				  "middle_name": "Masipag",
+				  "last_name": "Dela Cruz",
+				  "suffix_name": null,
+				  "gender": "M",
+				  "birthday": "2000-11-11",
+				  "birthplace": "Batangas",
+				  "religion": "Iglesia ni Cristo",
+				  "citizenship": "Filipino",
+				  "prev_school": "Basic Education School",
+				  "created": "2016-02-24 00:34:56",
+				  "modified": "2016-02-24 00:34:56"
+				}
+			  ]
 			}
 		);
 });
