c5567251022d7ff9422c1e5b702942d2c1281245
daveilarroyo <arroyo.daveil@gmail.com>
2021-02-02 18:48:26 +0800

    Link assssement in reports controler for test printing

--- api/controllers/reports_controller.php
+++ api/controllers/reports_controller.php
@@ -1,9 +1,10 @@
 <?php
 class ReportsController extends AppController{
 	var $name = 'Reports';
-	var $uses = null;
+	var $uses = array('Assessment');
 
 	function student_registration_form(){
+		$assessment = $this->Assessment->find('first');
 		$data = array();
 		$this->set(compact('data'));
 	}
