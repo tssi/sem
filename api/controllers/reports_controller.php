54ae30342bf31bcc0c62c39ffacc1a5bec67727b
SteplerPaulo <paulobiscocho@gmail.com>
2021-02-02 20:10:38 +0800

    Update Student Registration Form - Data

--- api/controllers/reports_controller.php
+++ api/controllers/reports_controller.php
@@ -1,11 +1,12 @@
 <?php
 class ReportsController extends AppController{
 	var $name = 'Reports';
-	var $uses = array('Assessment');
+	var $uses = array('Assessment','Student');
 
 	function student_registration_form(){
-		$assessment = $this->Assessment->find('first');
-		$data = array();
+		$this->Assessment->bindModel(array('belongsTo' => array('Student')));
+		$data = $this->Assessment->find('first');
+		//pr($data);exit;
 		$this->set(compact('data'));
 	}
 	
