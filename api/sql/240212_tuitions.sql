ff19eefd467e338abfe75e8b017656272f4c77cf
Dave Arroyo <arroyo.daveil@gmail.com>
2024-02-12 11:54:05 +0800

    Update tuitions table

---
+++ api/sql/240212_tuitions.sql
@@ -0,0 +1,2 @@
+ALTER TABLE tuitions MODIFY COLUMN name varchar(30) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL;
+ALTER TABLE tuitions MODIFY COLUMN description varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL;
