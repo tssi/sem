7daafe0bfa016767b63ead1490c4ff590754682f
Dave Arroyo <arroyo.daveil@gmail.com>
2024-02-13 09:45:45 +0800

    Tuition SY 2024 JHS

---
+++ api/sql/tuitions_202402130941.sql
@@ -0,0 +1,29 @@
+INSERT INTO tuitions (id,applicable_to,name,description,sy,year_level_id,tuition_per_hr,assessment_total) VALUES
+	 ('G07ERG24','NEW','G7 ER ID No. 24 ','Grade 7 Early Private ID No. 24 ',2024,'G7',0.00,37255.00),
+	 ('G07ERP24','NEW','G7 ER FPS ID No. 24','Grade 7 Early Public ID No. 24',2024,'G7',0.00,34255.00),
+	 ('G07FPS24','NEW','G7 New FPS ID No. 24','Grade 7 New Public ID No. 24',2024,'G7',0.00,37255.00),
+	 ('G07NEW24','NEW','G7 New ID No. 24','Grade 7 New Private ID No. 24',2024,'G7',0.00,40255.00),
+	 ('G08ERG24','NEW','G8 ER ID No. 24 ','Grade 8 Early Private ID No. 24 ',2024,'G8',0.00,37255.00),
+	 ('G08ERP24','NEW','G8 ER FPS ID No. 24','Grade 8 Early Public ID No. 24',2024,'G8',0.00,34255.00),
+	 ('G08FPS24','NEW','G8 New FPS ID No. 24','Grade 8 New Public ID No. 24',2024,'G8',0.00,37255.00),
+	 ('G08NEW24','NEW','G8 New ID No. 24','Grade 8 New Private ID No. 24',2024,'G8',0.00,40255.00),
+	 ('G09ERG24','NEW','G9 ER ID No. 24 ','Grade 9 Early Private ID No. 24 ',2024,'G9',0.00,37255.00),
+	 ('G09ERP24','NEW','G9 ER FPS ID No. 24','Grade 9 Early Public ID No. 24',2024,'G9',0.00,34255.00);
+INSERT INTO tuitions (id,applicable_to,name,description,sy,year_level_id,tuition_per_hr,assessment_total) VALUES
+	 ('G09FPS24','NEW','G9 New FPS ID No. 24','Grade 9 New Public ID No. 24',2024,'G9',0.00,37255.00),
+	 ('G09NEW24','NEW','G9 New ID No. 24','Grade 9 New Private ID No. 24',2024,'G9',0.00,40255.00),
+	 ('G10ERG24','NEW','G10 ER ID No. 24 ','Grade 0 Early Private ID No. 24 ',2024,'GX',0.00,37255.00),
+	 ('G10ERP24','NEW','G10 ER FPS ID No. 24','Grade 0 Early Public ID No. 24',2024,'GX',0.00,34255.00),
+	 ('G10FPS24','NEW','G10 New FPS ID No. 24','Grade 0 New Public ID No. 24',2024,'GX',0.00,37255.00),
+	 ('G10NEW24','NEW','G10 New ID No. 24','Grade 0 New Private ID No. 24',2024,'GX',0.00,40255.00),
+	 ('G11NEW24','NEW','G11 New ID No. 24','Grade 11 New ID No. 24',2024,'GY',0.00,45925.00),
+	 ('G11S2324','S23','G11 SNO 2023','Grade 11 SID No. 2023',2024,'GY',0.00,45925.00),
+	 ('G12NEW24','NEW','G12 New ID No. 24','Grade 12 New ID No. 24',2024,'GZ',0.00,45925.00),
+	 ('G12S2324','S23','G12 SNO 2023','Grade 12 SID No. 2023',2024,'GZ',0.00,32000.00);
+INSERT INTO tuitions (id,applicable_to,name,description,sy,year_level_id,tuition_per_hr,assessment_total) VALUES
+	 ('G8ES2324','S23','G8 ER ID No. 23','Grade 8 Early ID No. 23',2024,'G8',0.00,34225.00),
+	 ('G8OS2324','S23','G8 Old ID No. 23','Grade 8 Old ID No. 23',2024,'G8',0.00,37225.00),
+	 ('G9EB2224','B23','G9 ER ID No. 22 & B','Grade 9 Early ID No. 22 & Below',2024,'G9',0.00,31225.00),
+	 ('G9OB2224','B23','G9 Old ID No. 22 & B','Grade 9 Old ID No. 22 & Below',2024,'G9',0.00,34225.00),
+	 ('GXEB2224','B23','G10 ER ID No. 22 & B','Grade 10 Early  ID No. 22 & Below',2024,'GX',0.00,31225.00),
+	 ('GXOB2224','B23','G10 Old ID No. 22 & B','Grade 10 Old  ID No. 22 & Below',2024,'GX',0.00,34225.00);
