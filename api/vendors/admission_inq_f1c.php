27c5f1d980a310ed282909b8025dd42d4f8bd2d2
Dave Arroyo <arroyo.daveil@gmail.com>
2024-02-15 05:08:03 +0800

    Update f1c

--- api/vendors/admission_inq_f1c.php
+++ api/vendors/admission_inq_f1c.php
@@ -44,7 +44,7 @@ class AdmissionInquiryForm1C extends Formsheet{
 	function info(){
 		$metrics = array(
 			'base_x'=> 0.25,
-			'base_y'=> 1.3,
+			'base_y'=> 1.7,
 			'width'=> 8,
 			'height'=> 9,
 			'cols'=> 30,
@@ -53,13 +53,15 @@ class AdmissionInquiryForm1C extends Formsheet{
 		//$this->_colorful = true;
 		//$this->showLines = true;
 		$this->section($metrics);
-
-		$this->GRID['font_size']=9.5;
+		$this->GRID['font_size']=9;
+		$note = 'REMINDER: Kindly write legibly when completing the form to ensure accurate processing of your admission application. Some information has already been filled up from your initial inquiry. If there are corrections indicate the changes for updating.';
+		$this->wrapText(0,-2,$note,30,'l',0.85);
+		
 		$fields = array(
 				array('label'=>'STUDENT NAME','x'=>0,'y'=>0.75,'width'=>8,'no_box'=>true),
 				array('label'=>'First','x'=>0,'y'=>2,'width'=>8.5),
-				array('label'=>'Middle','x'=>9,'y'=>2,'width'=>8),
-				array('label'=>'Last','x'=>18,'y'=>2,'width'=>8),
+				array('label'=>'Middle','x'=>9,'y'=>2,'width'=>8.5),
+				array('label'=>'Last','x'=>18,'y'=>2,'width'=>8.5),
 				array('label'=>'Suffix','x'=>27,'y'=>2,'width'=>2.5),
 				array('label'=>'Grade/Level:','x'=>0,'y'=>5.75,'width'=>8, 'display'=>'inline','ix'=>3.25),
 				array('label'=>'Academic Track:','x'=>12,'y'=>5.75,'width'=>8, 'display'=>'inline','ix'=>16),
@@ -75,6 +77,23 @@ class AdmissionInquiryForm1C extends Formsheet{
 				array('label'=>'City','x'=>9,'y'=>20.5,'width'=>8),
 				array('label'=>'Province','x'=>18,'y'=>20.5,'width'=>8),
 				array('label'=>'Country','x'=>27,'y'=>20.5,'width'=>2.5),
+				array('label'=>'Guardian Name','x'=>0,'y'=>24,'width'=>8.5),
+				array('label'=>'Relationship to Student','x'=>9,'y'=>24,'width'=>8),
+				array('label'=>'Guardian Contact No.','x'=>18,'y'=>24,'width'=>8),
+				array('label'=>'PERMANENT ADDRESS','x'=>0,'y'=>30,'width'=>8,'no_box'=>true),
+				array('label'=>'Same as Current Address','x'=>9.75,'y'=>30,'width'=>2,'no_box'=>true, 'checkbox'=>true),
+				array('label'=>'No and Street Name','x'=>0,'y'=>31.5,'width'=>12),
+				array('label'=>'Subdivision','x'=>13,'y'=>31.5,'width'=>12),
+				array('label'=>'Barangay','x'=>0,'y'=>34.5,'width'=>8.5),
+				array('label'=>'City','x'=>9,'y'=>34.5,'width'=>8),
+				array('label'=>'Province','x'=>18,'y'=>34.5,'width'=>8),
+				array('label'=>'Country','x'=>27,'y'=>34.5,'width'=>2.5),
+				array('label'=>'Gender','x'=>0,'y'=>38,'width'=>2.5),
+				array('label'=>'Date of Birth','x'=>3,'y'=>38,'width'=>5.5,'watermark'=>'dd / mm / yyyy'),
+				array('label'=>'Citizenship','x'=>9,'y'=>38,'width'=>8),
+				array('label'=>'Religion','x'=>18,'y'=>38,'width'=>8),
+				array('label'=>'House Location Map (Sketch from the nearest landmark)','x'=>0,'y'=>42,'width'=>30,'height'=>12)
+
 		);
 
 		foreach($fields as $fld):
@@ -83,6 +102,11 @@ class AdmissionInquiryForm1C extends Formsheet{
 			$fw =  $fld['width'];
 			$fl =  $fld['label'];
 			$fh =  1.75;
+			if(isset($fld['height'])):
+				$fh = $fld['height'];
+			endif;
+			$this->GRID['font_size']=9.5;
+			$this->SetTextColor(0,0,0);
 			$this->leftText($fx,$fy,$fl,$fw,'');
 			if(!isset($fld['no_box'])):
 				if(!isset($fld['display'])):
@@ -91,6 +115,15 @@ class AdmissionInquiryForm1C extends Formsheet{
 					$fix = $fld['ix'];
 					$this->DrawBox($fix,$fy-1.2,$fw,$fh-0.25);
 				endif;
+				if(isset($fld['watermark'])):
+					$wmk = $fld['watermark'];
+					$this->GRID['font_size']=11;
+					$this->SetTextColor(160,160,160);
+					$this->centerText($fx,$fy+1.5,$wmk,$fw,'');
+				endif;
+			endif;
+			if(isset($fld['checkbox'])):
+				$this->DrawBox($fx-0.75,$fy-0.6,0.5,0.75);
 			endif;
 		endforeach;
 	}
